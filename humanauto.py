1d07cc561cba95e6ffe2f4a444c7c974d04aff9f
Назар Лакуста <n.lakusta@step.local>
2025-04-24 17:00:26 +0300

    Human commit 2

--- humanauto.py
+++ humanauto.py
@@ -8,6 +8,19 @@ class Auto:
         self.brand = brand
         self.passenger = []
 
+    def add_passenger(self,human):
+        self.passenger.append(human)
+
+    def print_passenger(self):
+        if self.passenger != []:
+            print(f"Names of {self.brand} passengers")
+
+            for passenger in self.passenger:
+                print(passenger.name)
+
+        else:
+            print(f"There are no passengers in {self.brand}")
+
 #Human
 nazar = Human("Nazar")
 maks = Human("Maks")
@@ -19,3 +32,12 @@ nikita = Human("Nikita")
 #Auto
 bus_Bogdan = Auto("Bogdan Bus")
 
+bus_Bogdan.add_passenger(nazar)
+bus_Bogdan.add_passenger(maks)
+bus_Bogdan.add_passenger(ruslanpotuzhnist)
+bus_Bogdan.add_passenger(evelinapotuzhna)
+bus_Bogdan.add_passenger(dimabarabas)
+bus_Bogdan.add_passenger(nikita)
+
+
+bus_Bogdan.print_passenger()
