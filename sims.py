d4041f7db1b779d426804f23c1084337da35e1af
Назар Лакуста <n.lakusta@step.local>
2025-04-24 17:40:04 +0300

    commit sims 2

--- sims.py
+++ sims.py
@@ -1,8 +1,8 @@
 from random import *
 
-class Human:
-    def __init__(self,name, job = None, home = None, car = None):
 
+class Human:
+    def __init__(self, name, job=None, home=None, car=None):
         self.name = name
         self.money = 100
         self.gladness = 50
@@ -12,13 +12,20 @@ class Human:
         self.car = car
 
     def get_home(self):
-        pass
+        self.home = House()
 
     def get_car(self):
-        pass
+        self.car = Auto(brands_of_car)
 
     def get_job(self):
-        pass
+        if self.car.drive():
+            pass
+        else:
+            self.to_repair()
+            return
+
+        self.job = Job(job_list)
+
 
     def eat(self):
         pass
@@ -26,7 +33,7 @@ class Human:
     def work(self):
         pass
 
-    def shopping(self,manage):
+    def shopping(self, manage):
         pass
 
     def chill(self):
@@ -38,18 +45,18 @@ class Human:
     def to_repair(self):
         pass
 
-    def days_index(self,day):
+    def days_index(self, day):
         pass
 
     def is_alive(self):
         pass
 
-    def live(self,day):
+    def live(self, day):
         pass
 
 
 class Auto:
-    def __init__(self,brand_list):
+    def __init__(self, brand_list):
         self.brand = choice(list(brand_list))
         self.fuel = brand_list[self.brand]["fuel"]
         self.strength = brand_list[self.brand]["strength"]
@@ -66,9 +73,31 @@ class Auto:
             print("The car cannot move")
             return False
 
+
 brands_of_car = {
-"BMW": {"fuel":100,"strength":100, "consumption":6},
-"Lada": {"fuel":50,"strength":40, "consumption":10},
-"Volvo": {"fuel":70,"strength":150, "consumption":8},
-"Ferrari": {"fuel":80,"strength":120, "consumption":14}
+    "BMW": {"fuel": 100, "strength": 100, "consumption": 6},
+    "Lada": {"fuel": 50, "strength": 40, "consumption": 10},
+    "Volvo": {"fuel": 70, "strength": 150, "consumption": 8},
+    "Ferrari": {"fuel": 80, "strength": 120, "consumption": 14}
+}
+
+
+class House:
+    def __init__(self):
+        self.mess = 0
+        self.food = 0
+
+
+
+class Job:
+    def __init__(self,job_list):
+        self.job = choice(list(job_list))
+        self.salary = job_list[self.job]["sallary"]
+        self.gladness_less = job_list[self.job]["gladness_less"]
+
+job_list = {
+    "Java developer": {"salary": 50, "gladness_less": 10},
+    "Python developer": {"salary": 40, "gladness_less": 3},
+    "Photoshop creator": {"salary": 30, "gladness_less": 25},
+    "C++ creator": {"salary": 60, "gladness_less": 30},
 }
