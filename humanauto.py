178bdf177b4e1fa1d41afcd7616762f1eca6f178
Назар Лакуста <n.lakusta@step.local>
2025-04-24 16:44:52 +0300

    Human commit 1

---
+++ humanauto.py
@@ -0,0 +1,21 @@
+class Human:
+    def __init__(self,name):
+        self.name = name
+
+
+class Auto:
+    def __init__(self,brand):
+        self.brand = brand
+        self.passenger = []
+
+#Human
+nazar = Human("Nazar")
+maks = Human("Maks")
+ruslanpotuzhnist = Human("Potuzhnist")
+evelinapotuzhna = Human("Potuzhna")
+dimabarabas = Human("Barabas")
+nikita = Human("Nikita")
+
+#Auto
+bus_Bogdan = Auto("Bogdan Bus")
+
