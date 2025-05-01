ffc35fca08e4e7e3fe50eee7519d6e515de2acfd
Назар Лакуста <n.lakusta@step.local>
2025-05-01 17:20:56 +0300

    uspadkubannya super2

--- uspadkuvannyasuper.py
+++ uspadkuvannyasuper.py
@@ -11,3 +11,27 @@ class Human:
               f"\nHeight: {self.height}"
               f"\nAge: {self.age}"
               f"\nColor hair: {self.color_hair}")
+
+
+class Worker(Human):
+
+    def __init__(self,name,height,age,color_hair, salary, position, experience):
+            super().__init__(name,height,age,color_hair)
+
+            self.salary = salary
+            self.position = position
+            self.experience = experience
+
+    def info(self):
+        super().info()
+        print(f"Worker info:"
+              f"\nSalary: {self.salary}"
+              f"\nPosition: {self.position}"
+              f"\nExperience: {self.experience}")
+
+
+nazar = Human("Nazar",175,21,"Black")
+nazar.info()
+
+ruslan = Worker("Ruslan",180,13,"Brown",1,"Potuzhnist",1)
+ruslan.info()
