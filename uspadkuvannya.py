d575f46bf69011fc082a8f45c33d1fb4a75ef617
Назар Лакуста <n.lakusta@step.local>
2025-05-01 16:49:39 +0300

    uspadkubannya

---
+++ uspadkuvannya.py
@@ -0,0 +1,22 @@
+# батьківський клас
+class Human:
+    height = 170
+
+
+# дочірний клас
+class Student(Human):
+    pass
+
+# дочірний клас
+class Worker(Human):
+    pass
+
+
+
+nazar = Student()
+dima = Worker()
+
+
+
+
+
